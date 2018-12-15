6cef8c47783bcb897bd29d133ee713557b0c276c
Skyelah <skyelahbay@gmail.com>
2018-12-15 11:20:18 -0600

    initial commit and pushing of current progress to github.

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LandingPage.scss';
+
+const LandingPage = () => {
+  return (
+    <div id="landing-page">
+      <header className="header">
+        some text...
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
