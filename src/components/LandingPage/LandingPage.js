83288597999d347cfcebdc69f67d403d0ea2141d
Skyelah <skyelahbay@gmail.com>
2018-12-21 21:25:10 -0600

    refactored SCSS to use a function for color reference, improved readability. Also started following BEM style conventions.

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,17 +5,17 @@ const LandingPage = () => {
   return (
     <div id="landing-page">
       <header className="header">
-        <div className="logo-box">
-          <img src="/images/logo-white.png" alt="Logo" className='logo' />
+        <div className="header__logo-box">
+          <img src="/images/logo-white.png" alt="Logo" className='header__logo' />
         </div>
 
         <div className="cta-container">
           <h1 className="heading-primary">
-            <span className="heading-primary-top">Outdoors</span>
-            <span className="heading-primary-bottom">It's where we are</span>
+            <span className="heading-primary--top">Outdoors</span>
+            <span className="heading-primary--bottom">It's where we are</span>
           </h1>
           
-          <a href="#" className='btn btn-white btn-animated'>Discover The Outdoors</a>
+          <a href="#" className='btn btn--white btn--animated'>Discover The Outdoors</a>
         </div>
         
       </header>
